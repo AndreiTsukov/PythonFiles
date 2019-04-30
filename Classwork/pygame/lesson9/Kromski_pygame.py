bab72b76ca274258d947fc3cbbce878b7dc4cf87
pupil <pupil@ivkhk.ee>
2019-04-30 11:07:22 +0300

    lesson9/Kromski_pygame движение объекта по слоям

--- Classwork/pygame/lesson9/Kromski_pygame.py
+++ Classwork/pygame/lesson9/Kromski_pygame.py
@@ -1,3 +1,4 @@
+'''
 import pygame
 #зарускаем программу
 pygame.init()
@@ -26,4 +27,39 @@ while done:
                if event.type == pygame.QUIT:
                      done=False
        clock.tick(20)
+pygame.quit()
+'''
+from random import randint
+import pygame
+pygame.init()
+sc = pygame.display.set_mode((400, 400))
+background = pygame.Surface((400, 200))
+background.fill((0, 255, 0))
+xb = 0
+yb = 100
+hero = pygame.Surface((100, 100))
+hero.fill((255, 0, 0))
+x = 0
+y = 50
+# порядок прорисовки важен!
+background.blit(hero, (x, y))
+sc.blit(background, (xb, yb))
+pygame.display.update()
+done = True
+while done:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            done = False
+        elif i.type == pygame.MOUSEBUTTONUP:
+            yb = randint(0, 200)
+    if x < 400:
+        x += 2
+    else:
+        x = 0
+    sc.fill((0, 0, 0))
+    background.fill((0, 255, 0))
+    background.blit(hero, (x, y))
+    sc.blit(background, (xb, yb))
+    pygame.display.update()
+    pygame.time.delay(30)
 pygame.quit()
