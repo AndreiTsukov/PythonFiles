a5a567c7985adcd763d241da1884efcc0c1f995d
pupil <pupil@ivkhk.ee>
2019-04-30 11:03:40 +0300

    lesson9/Kromski_pygame слои(более наглядно)

--- Classwork/pygame/lesson9/Kromski_pygame.py
+++ Classwork/pygame/lesson9/Kromski_pygame.py
@@ -7,15 +7,20 @@ white=( 255, 255, 255)
 green=(   0, 255,   0)
 red=( 255,   0,   0)
 size = [700,700]
-screen=pygame.display.set_mode(size)
+sc=pygame.display.set_mode(size)
 pygame.display.set_caption("Professor Craven's Cool Game")
 done = True
 clock=pygame.time.Clock()
-
-surf = pygame.Surface((200,150))
-surf.fill(white)
-screen.blit(surf, (50,25))
-pygame.display.flip()
+sc = pygame.display.set_mode((300, 200))
+surf = pygame.Surface((200, 150))
+surf.fill((255, 255, 255))
+surf.set_alpha(200)
+# сначала на главной поверхности рисуется зеленый прямоуг.
+pygame.draw.rect(sc, (0, 255, 0), (0, 80, 300, 40))
+# затем другая поверхность,
+# она будет поверх прямоугольника
+sc.blit(surf, (50, 25))
+pygame.display.update()
 while done:
        for event in pygame.event.get():
                if event.type == pygame.QUIT:
