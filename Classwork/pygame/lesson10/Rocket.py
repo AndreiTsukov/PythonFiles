4ac5fe16c4af818942e9bc41fbd40d1883358f91
pupil <pupil@ivkhk.ee>
2019-05-02 10:49:26 +0300

    working lesson10/Rocket.py (comments and variables not TRUE! )

--- Classwork/pygame/lesson10/Rocket.py
+++ Classwork/pygame/lesson10/Rocket.py
@@ -7,9 +7,9 @@ BLACK = (0, 0, 0)
 class Rocket:
     # ширина и высота у всех
     # экземпляров-ракет будут одинаковы
-    width_rocket = 20
-    height_rocket = 50
-    def __init__(self, surface, color):
+    width_rocket = 50
+    height_rocket = 20
+    def __init__(self, surface, color,j):
         """Конструктору необходимо передать поверхность, по которой
         будет летать ракета и цвет самой ракеты"""
         self.surf = surface
@@ -19,19 +19,29 @@ class Rocket:
         # Координаты верхнего левого угла ракеты устанавливаются так,
         # чтобы ракета летела ровно по центру поверхности по горизонтали
         # и появлялась снизу.
-        self.x = surface.get_width()
-        self.y = surface.get_height()//2 - Rocket.width_rocket//2
-    def fly(self):
+        if j == 1:
+            self.x = 0
+            self.y = surface.get_height()//2 - Rocket.width_rocket//2
+        else:
+            self.x = surface.get_width()
+            self.y = surface.get_height()//2 - Rocket.width_rocket//2
+    def fly(self,j):
         """Вызов метода fly() поднимает ракету на 3 пикселя.
         Если ракета скрывается вверху, она снова появится снизу"""
-        pygame.draw.rect(self.surf, self.color, (self.x, self.y,
-        Rocket.width_rocket, Rocket.height_rocket))
-        self.x -= 3
+        #pygame.draw.polygon(self.surf, self.color, ([100,100], [50,200],[150,200]))
+        pygame.draw.rect(self.surf, self.color, (self.x, self.y,Rocket.width_rocket, Rocket.height_rocket))
+        if j == 1:
+            self.x += 3
+            if self.x > WIN_WIDTH:
+                self.x = 0
+        else:
+            self.x -= 3
+            if self.x < -Rocket.width_rocket:
+                # Поэтому перебрасываем ракету под нижнюю границу окна.
+                self.x = WIN_WIDTH
         # Если координата y ракеты уходит за -50, то значит она
         # полностью скрылась вверху.
-        if self.x < -Rocket.width_rocket:
-            # Поэтому перебрасываем ракету под нижнюю границу окна.
-            self.x = WIN_HEIGHT
+
 sc = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 # левая белая поверхность, равная половине окна
 surf_left = pygame.Surface((WIN_WIDTH, WIN_HEIGHT//2))
@@ -44,12 +54,14 @@ sc.blit(surf_left, (0, 0))
 sc.blit(surf_right, (0, WIN_HEIGHT//2))
 # создаем черную ракету для левой поверхности
 # и белую - для правой
-rocket_left = Rocket(surf_left, BLACK)
-rocket_right = Rocket(surf_right, WHITE)
+j = 0
+rocket_left = Rocket(surf_left, BLACK,j)
+rocket_right = Rocket(surf_right, WHITE,j)
 # какая половина активна, до первого клика - никакая
 active_left = False
 active_right = False
 done = True
+
 while done:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
@@ -57,11 +69,11 @@ while done:
         elif i.type == pygame.MOUSEBUTTONUP:
         # если координата X клика меньше половины окна,
         # т. е. клик произошел в левой половине ...
-            if i.pos[0] < WIN_WIDTH//2:
+            if i.pos[1] < WIN_HEIGHT//2:
                 # то активируем левую, отключаем правую
                 active_left = True
                 active_right = False
-            elif i.pos[0] > WIN_WIDTH//2:
+            elif i.pos[1] > WIN_HEIGHT//2: # pos = [x,y] , x - 0  y - 1
                 # иначе - наоборот
                 active_right = True
                 active_left = False
@@ -70,14 +82,16 @@ while done:
         # то заливаем только ее цветом,
         surf_left.fill(WHITE)
         # поднимаем ракету,
-        rocket_left.fly()
+        j = 1
+        rocket_left.fly(j)
         # заново отрисовываем левую поверхность на главной.
         sc.blit(surf_left, (0, 0))
     elif active_right:
+        j = 0
         # Если активна правая -> аналогично
         surf_right.fill(BLACK)
-        rocket_right.fly()
-        sc.blit(surf_right, (0,WIN_HEIGHT//2))
+        rocket_right.fly(j)
+        sc.blit(surf_right, (0, WIN_HEIGHT//2))
     pygame.display.update()
     pygame.time.delay(20)
 pygame.quit()
